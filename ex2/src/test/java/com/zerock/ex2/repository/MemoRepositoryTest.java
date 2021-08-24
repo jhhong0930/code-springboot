72daa81aea0c2c28e716c79269ecadf1b146fa34
Joe <jhhong0930@gmail.com>
2021-08-25 08:16:17 +0900

    페이징 처리 테스트

--- ex2/src/test/java/com/zerock/ex2/repository/MemoRepositoryTest.java
+++ ex2/src/test/java/com/zerock/ex2/repository/MemoRepositoryTest.java
@@ -4,6 +4,10 @@ import com.zerock.ex2.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -77,6 +81,61 @@ class MemoRepositoryTest {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    /**
+     * 페이징 처리 테스트
+     * of(int page, int size): 0부터 시작하는 페이지 번호와 개수, 정렬이 지정되지 않음
+     * of(int page, int size, Sort.Direction direction, String...props):
+     * - 0부터 시작하는 페이지 번호와 개수, 정렬의 방향과 정렬 기준 필드들
+     * of(int page, int size, Sort sort): 페이지 번호와 개수, 정렬 관련 정보
+     */
+    @Test
+    void testPageDefault() {
+
+        // 1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        System.out.println("===============================");
+
+        // 페이지 총 개수
+        System.out.println("result.getTotalPages() = " + result.getTotalPages());
+        // 전체 개수
+        System.out.println("result.getTotalElements() = " + result.getTotalElements());
+        // 현재 페이지 번호 0부터 시작
+        System.out.println("result.getNumber( = " + result.getNumber());
+        // 페이지당 데이터 개수
+        System.out.println("result.getSize() = " + result.getSize());
+        // 다음 페이지 존재 여부
+        System.out.println("result.hasNext() = " + result.hasNext());
+        // 시작 페이지(0) 여부
+        System.out.println("result.isFirst() = " + result.isFirst());
+
+        System.out.println("===============================");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+
+    }
+
+    @Test
+    void testSort() {
+
+        Sort sort1 = Sort.by("mno").descending();
+
+        Pageable pageable = PageRequest.of(2, 10, sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+
     }
 
 }
