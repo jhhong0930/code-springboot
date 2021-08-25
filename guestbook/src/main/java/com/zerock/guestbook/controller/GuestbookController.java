9a7a356a2b37d1607ad842e37a9d377b26546a0f
Joe <jhhong0930@gmail.com>
2021-08-26 08:47:29 +0900

    등록 페이지와 등록 처리

--- guestbook/src/main/java/com/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/zerock/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.zerock.guestbook.controller;
 
+import com.zerock.guestbook.dto.GuestbookDTO;
 import com.zerock.guestbook.dto.PageRequestDTO;
 import com.zerock.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -32,4 +35,24 @@ public class GuestbookController {
 
     }
 
+    @GetMapping("/register")
+    public void register() {
+
+        log.info("register");
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+
+        log.info("dto={}", dto);
+
+        // 새로 추가된 엔테테의 번호
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg",gno);
+
+        return"redirect:/guestbook/list";
+
+    }
 }
