a1d150d34b3d5aca4d23a2c88316cc682e917e89
Joe <jhhong0930@gmail.com>
2021-08-29 23:23:43 +0900

    Create project club

---
+++ club/src/main/java/com/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.zerock.club.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
